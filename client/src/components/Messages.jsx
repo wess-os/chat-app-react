ecf0305ef7d7761b2110e32370512e381c3e4c06
Wesley <wes.lol25@gmail.com>
2024-09-04 15:58:07 -0300

    style: new component Messages

---
+++ client/src/components/Messages.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Messages() {
+    return (
+        <Container>
+            Mensagens
+        </Container>
+    );
+}
+
+const Container = styled.div`
+    height: 80%;
+`;
