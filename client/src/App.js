3380d4164b3b17abc7d61b82bd3980d95037d693
Wesley <wes.lol25@gmail.com>
2024-09-04 15:57:17 -0300

    feat: new route setAvatar

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Chat from "./pages/Chat";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SetAvatar from "./pages/SetAvatar";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/setAvatar" element={<SetAvatar />} />
         <Route path="/" element={<Chat />} />
       </Routes>
     </BrowserRouter>
