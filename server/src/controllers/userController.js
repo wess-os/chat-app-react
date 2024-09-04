0bf887187d3d910e53470a5de9089dd49aa76cc7
Wesley <wes.lol25@gmail.com>
2024-09-04 15:59:53 -0300

    feat: new file userController

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -48,3 +48,39 @@ module.exports.login = async (req, res, next) => {
         next(error);
     }
 };
+
+module.exports.setAvatar = async (req, res, next) => {
+    try {
+        const userId = req.params.id;
+        const avatarImage = req.body.image;
+
+        const userData = await User.findByIdAndUpdate(
+            userId, {
+                isAvatarImageSet: true,
+                avatarImage,
+            }
+        );
+
+        return res.json({ 
+            isSet: userData.isAvatarImageSet, 
+            image: userData.avatarImage 
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports.getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find({ _id: { $ne: req.params.id } }).select([
+            "email",
+            "username",
+            "avatarImage",
+            "_id",
+        ]);
+
+        return res.json(users);
+    } catch (error) {
+        next(error);
+    }
+}
